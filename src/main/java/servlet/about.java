5dbe3fe4d859b92143503264fecdd150bb02b7be
ngoctrung0962 <84114824+ngoctrung0962@users.noreply.github.com>
2021-10-22 16:23:42 +0700

    them trang about? con thieu

---
+++ src/main/java/servlet/about.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.LoaiSP;
+import beans.SanPham;
+import beans.SanPhamInCart;
+import conn.ConnectionUtils;
+import utils.DBUtils;
+
+/**
+ * Servlet implementation class home
+ */
+@WebServlet(urlPatterns = "/about", name = "about")
+public class about extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public about() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        Connection conn;
+        try {
+            conn = ConnectionUtils.getConnection();
+            new cart().doPost(request, response);
+            request.getRequestDispatcher("/WEB-INF/views/about.jsp").forward(request, response);
+        } catch (ClassNotFoundException | SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
